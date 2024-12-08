89beb435d533d78e9728b629b08e455ee49f20bf
naoto.kido <naoto.kido@techblue.style>
2024-12-08 23:07:45 +0900

    R2に保存するよう変更

---
+++ microservice/qr/src/main/java/com/naoido/resources/QrCodeGenerateResource.java
@@ -0,0 +1,36 @@
+package com.naoido.resources;
+
+import com.naoido.dto.QrCodeGenerateDTO;
+import com.naoido.services.QrCodeService;
+import jakarta.validation.Valid;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Path("/qrcode")
+public class QrCodeGenerateResource {
+
+    @POST
+    @Path("/generate")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response generateQrCode(@Valid QrCodeGenerateDTO qrCodeGenerateDTO) {
+        Map<String, Object> result = new HashMap<>();
+        try {
+            String qrcode = QrCodeService.generateAndSave(qrCodeGenerateDTO.getUserId(), qrCodeGenerateDTO.getContent());
+            result.put("qrcode", qrcode);
+            result.put("status", "success");
+
+            return Response.status(Response.Status.OK).entity(result).build();
+        } catch (Exception e) {
+            result.put("message", "Failed to generate QR Code");
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(result).build();
+        }
+    }
+}
