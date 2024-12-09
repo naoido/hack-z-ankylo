87e0ec36c3b0aa9bffb1f5b9a230c4e4487cd979
thirdlf03 <114989748+thirdlf03@users.noreply.github.com>
2024-12-10 02:41:13 +0900

    悲しみの海

--- mobile/app/lib/supabase.ts
+++ mobile/app/lib/supabase.ts
@@ -1,7 +1,7 @@
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = "https://ouzevkyfyamaapswwsjm.supabase.co"
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL
 const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
