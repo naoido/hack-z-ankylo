b9acadd3bac078fb1d875772e5edcc2d485d562c
naoto.kido <naoto.kido@techblue.style>
2024-12-12 02:38:30 +0900

    QRCodeリストを取得するエンドポイント追加

---
+++ workers/hackz-ankylo-wk/src/models/qrcode.ts
@@ -0,0 +1,18 @@
+import { z } from '@hono/zod-openapi';
+
+export const QrCode = z.object({
+    qrcode_id: z.string().openapi({
+        example: "12345678-e29b-41d4-a716-123456789000"
+    }),
+    qrcode_name: z.string().openapi({
+        example: "example.com"
+    }),
+    qrcode_content: z.string().openapi({
+        example: "https://example.com"
+    }),
+    user_id: z.string().openapi({
+        example: "12345678-e29b-41d4-a716-123456789000"
+    })
+}).openapi("QrCode");
+
+export const QrCodeList = z.array(QrCode).openapi('QrCodeList');
